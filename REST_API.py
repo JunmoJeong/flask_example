7d139dbf16c0c486f3c2f7289b68ca58f751e642
JunmoJeong <jjmstars7@gmail.com>
2022-07-16 17:07:39 +0900

    flask

--- REST_API.py
+++ REST_API.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello, World!"
+    return '''
+    <h1> 이건 h1 제목</h1>
+    <p> 이건 본문 p </p>
+    <a href="https://www.naver.com"> Naver 홈페이지 바로가기</a>
+    '''
 
 
 @app.route('/user/<user_name>/<int:user_id>')
 def user(user_name, user_id):
-    return f'Hello, {user_name} ({user_id})!'
+    return f'Hello, {user_name}({user_id})!'
 
 
 if __name__ == '__main__':
