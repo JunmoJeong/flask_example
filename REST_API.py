e3f97cbf5e93bcd93f45dcb837a20f68249a693f
JunmoJeong <jjmstars7@gmail.com>
2022-07-16 16:59:48 +0900

    flask

--- REST_API.py
+++ REST_API.py
@@ -1,13 +1,18 @@
-from asyncio.base_futures import _format_callbacks
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return "hello world!"
+    return "Hello, World!"
 
 
-if __name__ == "__main__":
+@app.route('/user/<user_name>/<int:user_id>')
+def user(user_name, user_id):
+    return f'Hello, {user_name} ({user_id})!'
+
+
+if __name__ == '__main__':
     app.run(debug=True)
